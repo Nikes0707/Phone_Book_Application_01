06e8d203f9099e6faa6bd739062a1d0356ec478b
Nikheel <nikheelBodkhe1999@gmail.com>
2022-08-24 12:06:33 +0530

    KIR1:Create Project and package structure

---
+++ src/main/java/com/NikheelIT/Phone_Book_Application/PhoneBookApplication01Application.java
@@ -0,0 +1,13 @@
+package com.NikheelIT.Phone_Book_Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneBookApplication01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneBookApplication01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/NikheelIT/Phone_Book_Application/PhoneBookApplication01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NikheelIT.Phone_Book_Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneBookApplication01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
